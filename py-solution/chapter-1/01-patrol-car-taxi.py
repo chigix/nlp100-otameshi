63e5387a4a8b3e3f08b68a98b20da333c1748508
Richard Lea <chigix@zoho.com>
2018-09-06 20:49:37 +0900

    New: scripts for ch1-0,1-1,1-2

---
+++ py-solution/chapter-1/01-patrol-car-taxi.py
@@ -0,0 +1,5 @@
+ORIGINAL_STR = 'パタトクカシーー';
+if len(ORIGINAL_STR) != 8:
+  raise Exception('The length is not equal to 8 as expected')
+
+print(ORIGINAL_STR[0] + ORIGINAL_STR[2] + ORIGINAL_STR[4] + ORIGINAL_STR[6]);
---
+++ py-solution/chapter-1/02-alternate-characters-combining.py
@@ -0,0 +1,18 @@
+# coding: UTF-8
+
+from pprint import pprint
+import unittest
+
+str1 = 'パトカー'
+str2 = 'タクシー'
+
+class TestSuite(unittest.TestCase):
+
+  def testCase(self):
+    zipped = zip(str1, str2)
+    self.assertEqual(
+      [('パ', 'タ'), ('ト', 'ク'), ('カ', 'シ'), ('ー', 'ー')],
+      list(zipped))
+    pprint(''.join(map(lambda arr: arr[0] + arr[1], zip(str1,str2))))
+
+unittest.main()
